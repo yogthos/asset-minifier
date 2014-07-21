613c42c5be78841e6f18bba0c683a2d2070ed113
Yogthos <dmitri.sotnikov@gmail.com>
2014-07-21 18:17:04 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject asset-minifier "0.1.1"
+(defproject asset-minifier "0.1.2"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
