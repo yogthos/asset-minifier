5feff05f047cd843ecdf22712eb6ed97abd078e9
Blake Miller <blake@tengrade.com>
2014-12-19 14:03:46 -0800

    update google closure compiler dependency, fix JSSourceFile -> SourceFile
    JSSourceFile was renamed, asset minification with v20141120 fails with
    java.lang.ClassNotFoundException: com.google.javascript.jscomp.JSSourceFile
    
    this commit updates the closure compiler dependency, bumps the version to
    0.1.5-SNAPSHOT, and renames s/JSSourceFile/SourceFile/g

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject asset-minifier "0.1.4"
+(defproject asset-minifier "0.1.5-SNAPSHOT"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.7" :exclusions [rhino/js]]
-                 [com.google.javascript/closure-compiler "v20131014"]
+                 [com.google.javascript/closure-compiler "v20141120"]
                  [commons-io "2.4"]])
