9f97a52e74571cde26a8da6cf54796ed024bf855
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-11-26 09:40:19 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject asset-minifier "0.1.6"
+(defproject asset-minifier "0.1.7"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
