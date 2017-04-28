350bf9f82d395bab5e958685dc17c69bc859b070
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-04-28 09:08:41 -0400

    bumped up closure compiler version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject asset-minifier "0.2.1"
+(defproject asset-minifier "0.2.2"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
-                 [com.google.javascript/closure-compiler "v20161201"]
+                 [com.google.javascript/closure-compiler "v20160208"]
                  [commons-io "2.5"]]
   :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
                    :injections [(require 'pjstadig.humane-test-output)
