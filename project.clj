effd229a74c1e6499172b822be791cf6ac0d0c0d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2018-03-18 08:38:19 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject asset-minifier "0.2.4"
+(defproject asset-minifier "0.2.6"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
                  [com.google.javascript/closure-compiler "v20160208"]
                  [clj-html-compressor "0.1.1"]
-                 [commons-io "2.5"]]
-  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
+                 [commons-io "2.6"]]
+  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]}})
