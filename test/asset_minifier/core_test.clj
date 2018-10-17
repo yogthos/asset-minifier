03db0982eda8e017cc46f816bf07ddb3973a1d74
Abel VARGA <avarga@whitepages.com>
2018-10-17 22:18:30 +0200

    Add specs

--- test/asset_minifier/core_test.clj
+++ test/asset_minifier/core_test.clj
@@ -67,13 +67,15 @@
 
     ;; minify directory
     (run-test
-     (minify-js input-path (str output-path "output.min.js"))
+     (minify-js input-path (str output-path "output.min.js")
+                {:language-in :ecmascript6
+                 :language-out :ecmascript5})
      {:warnings ()
       :errors ()
-      :sources ["externs.js" "input1.js" "input2.js"]
+      :sources ["externs.js" "input1.js" "input2.js", "input3.js"]
       :target "output.min.js"
-      :original-size 2547
-      :compressed-size 1804
+      :original-size 2586
+      :compressed-size 1841
       :gzipped-size 807})
 
     ;; minify a file
@@ -115,7 +117,7 @@
       :sources #{"input2.js"},
       :target "output.min.js",
       :original-size 2409,
-      :compressed-size 1265,
+      :compressed-size 1418,
       :gzipped-size 600
       :warnings
       ["JSC_UNDEFINED_EXTERN_VAR_ERROR. name hljs is not defined in the externs. at test/resources/js/externs.js line 1 : 0"]}))
@@ -125,14 +127,14 @@
      (minify-html (str input-path "/html/") (str output-path "html/") {})
      {:sources '("testCompress.html") :targets '("testCompress.html") :original-size 581 :compressed-size 459 :gzipped-size 256}))
 
-  (testing "minify function" 
+  (testing "minify function"
     (is (= (minify [[:html {:source (str input-path "/html/") :target (str output-path "html/")}]
                     [:js {:source (str input-path "/js/input1.js") :target (str output-path "output.min.js")}]
                     [:css {:source (str input-path "/css/input1.css") :target (str output-path "output.min.css")}]])
-            [{:sources '("testCompress.html") 
-              :targets '("testCompress.html") 
-              :original-size 581 
-              :compressed-size 459 
+            [{:sources '("testCompress.html")
+              :targets '("testCompress.html")
+              :original-size 581
+              :compressed-size 459
               :gzipped-size 256}
              {:warnings ()
               :errors ()
