09bc01a50a759bbc6ee7f308f9c79393e3ae5e95
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-03-04 19:19:08 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject asset-minifier "0.2.6"
+(defproject asset-minifier "0.2.7"
   :description "a library to minify CSS and Js sources"
   :url "https://github.com/yogthos/asset-minifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojure-future-spec "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clojure-future-spec "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
                  [com.google.javascript/closure-compiler "v20160208"]
                  [clj-html-compressor "0.1.1"]
                  [commons-io "2.6"]]
-  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
+  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.9.0"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]}})
